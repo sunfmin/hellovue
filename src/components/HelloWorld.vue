0f22b0fce202893a027d8653fef5155005021452
Felix Sun <sunfmin@gmail.com>
2021-08-16 09:54:02 +0800

    add v-fieldname

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,69 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="helloworld">
+    <div class="a">
+      <input :value="value1" @input="onInput($event)"/>
+      {{value1}}
+    </div>
+    
+    <div class="b">
+      <input v-model="value2"/>
+      {{value2}}
+    </div>
+
+    <div class="c">
+      <input v-fieldname='"value3"'/>
+      {{value3}}
+    </div>
+
+    <div class="d">
+      <v-text-field v-fieldname='"value4"'/>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  directives: {
+    fieldname: {
+      inserted: function(el, binding, vnode) {
+        console.log(el, binding, vnode)
+        console.log("vnode.context.$el", vnode.context.$el)
+        console.log("vnode.context", vnode.context)
+        console.log("this", this)
+
+        vnode.context.$on("test", function(evt) {
+          console.log("test evt", evt)
+        })
+
+        if (vnode.componentInstance) {
+          vnode.componentInstance.$on("input", function(value) {
+            console.log("componentInstance evt", value)
+          })
+        }
+
+      }
+    }
+  },
+  methods: {
+
+    onInput: function(evt) {
+      console.log("this", this)
+
+      // console.log("this.$listeners", this.$listeners)
+      // console.log("evt", evt)
+      this.$emit("input", evt.target.value)
+      this.$emit("test", "123")
+      // this.value1 = evt.target.value
+    },
+  },
+  data: function() {
+    return {
+      value1: "123",
+      value2: "",
+      value3: "",
+      value4: "222"
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
